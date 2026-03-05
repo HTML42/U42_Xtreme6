9e04322d8f7c4559d1d4940a36c4a0d619e907f3
Mark Paspirgilis <mp@power-of-m.de>
2026-03-05 23:52:06 +0100

    remove old files

--- dist/objects.test.js
+++
@@ -1,9 +0,0 @@
-/* SOURCE: objects/x_user/x_user.test.js */
-// platzhalter: test-runner wird später integriert.
-// regel: keine imports/require/exports in dieser datei.
-console.log('x-user.test.js placeholder');
-
-/* SOURCE: objects/x_users/x_users.test.js */
-// platzhalter: test-runner wird später integriert.
-// regel: keine imports/require/exports in dieser datei.
-console.log('x-users.test.js placeholder');
